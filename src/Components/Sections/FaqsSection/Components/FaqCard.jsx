7c7b16a1a66b0a9d77edf39c026423eed31f32d7
Mustafa Sayed <mustafacoder9@gmail.com>
2025-03-16 18:58:04 +0200

    Make a Smoothly Arrow State ( Faqs Section => Faq Card )

--- src/Components/Sections/FaqsSection/Components/FaqCard.jsx
+++ src/Components/Sections/FaqsSection/Components/FaqCard.jsx
@@ -19,8 +19,8 @@ function FaqCard({ faqData }) {
                 {/* Question */}
                 <h3 className='font-medium flex-1'>{question}</h3>
                 {/* Arrow State */}
-                <div className='w-10 h-10 leading-10 text-center rounded-full border border-dark-color-12 text-xl'>
-                    <i className={`fa-solid fa-angle-${isOpen ? 'up' : 'down'}`}></i>
+                <div className={`w-10 h-10 leading-10 text-center rounded-full border border-dark-color-12 text-xl transition-transform ${isOpen ? '-rotate-180' : ''}`}>
+                    <i className='fa-solid fa-angle-down'></i>
                 </div>
             </div>
             {/* Answer */}
